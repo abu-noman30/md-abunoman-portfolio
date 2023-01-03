f8040974031551070c0830f09bedc1e3408926d9
noman <nomannabil30@gmail.com>
2023-01-04 02:03:30 +0600

    about container changes

--- src/Shared/About/About.js
+++ src/Shared/About/About.js
@@ -4,26 +4,26 @@ const About = () => {
 	return (
 		<>
 			{/* About Container */}
-			<div
-				className='about-container w-10/12 mx-auto mb-10  lg:-mb-60'
-				id='aboutme-id'
-			>
+			<div className='about-container w-10/12 mx-auto mb-16 '>
 				<div className='flex flex-col items-center justify-center space-y-3 mb-10'>
 					<h1 className='text-3xl font-semibold text-white capitalize lg:text-4xl text-center drop-shadow-lg shadow-white'>
 						LET ME INTRODUCE MYSELF
 					</h1>
 					<progress className='progress w-72 text-blue-500'></progress>
 				</div>
-				<div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
-					<div className='col-span-2 flex flex-col items-center justify-center mb-0 lg:mb-10'>
-						<div className='avatar w-48 rounded ring ring-primary ring-offset-base-100 ring-offset-2 mt-8 '>
-							<img src='Images/Noman(5).jpg' alt='' />
-						</div>
-						<h1 className='bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-2xl lg:text-5xl font-extrabold text-transparent  my-5 text-center'>
-							MD. ABU NOMAN
-						</h1>
+				{/* Image Container */}
+				<div className='col-span-2 flex flex-col items-center justify-center mb-0 lg:mb-10'>
+					<div className='avatar w-48 rounded ring ring-primary ring-offset-base-100 ring-offset-2 mt-8 '>
+						<img src='Images/Noman(5).jpg' alt='' />
 					</div>
-					<div className='avatar col-span-2 lg:col-span-1  flex flex-col italic pt-6 -mt-8 lg:mt-0 '>
+					<h1 className='bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-2xl lg:text-5xl font-extrabold text-transparent  my-5 text-center'>
+						MD. ABU NOMAN
+					</h1>
+				</div>
+
+				{/* About */}
+				<div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+					<div className='flex flex-col italic pt-6 mb-10  '>
 						<div className='  space-y-2'>
 							<p className='text-zinc-300'>
 								<span className=' text-white font-bold underline underline-offset-2  '>
@@ -63,9 +63,9 @@ const About = () => {
 							</p>
 						</div>
 					</div>
-					<div className='avatar col-span-2 lg:col-span-1 -mt-8 lg:mt-0'>
+					<div className='text-justify'>
 						<blockquote>
-							<span className='text-lg italic font-serif text-zinc-400'>
+							<span className='text-lg italic font-serif text-zinc-400 '>
 								Working with Full Stack Web Development(MERN) to build Websites
 								that are User-Friendly on the Front-end and Server-side. Staying
 								up-to-date with modern Web technologies is always a Priority for
